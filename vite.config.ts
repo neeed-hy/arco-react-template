a4d0744ce197ff81768247f0a7627c57aebc18ad
he.yu <he.yu@sfit.shfe.com.cn>
2023-10-10 13:08:31 +0800

    feat: 🎸 预留接口转发目标

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import vitePluginForArco from '@arco-plugins/vite-react';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
 import setting from './src/settings.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [
     react(),
