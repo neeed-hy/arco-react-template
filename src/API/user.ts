b991ed3bce094c4af35431771b6e54ee96da0b57
he.yu <he.yu@sfit.shfe.com.cn>
2023-10-18 09:59:04 +0800

    feat: 🎸 替换axios

--- src/API/user.ts
+++ src/API/user.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-// 默认的mockjs不能拦截fetch，因此在mock阶段需使用axios；后续解决这个问题
-import axios from 'axios';
 
 import { IRes } from '@/types/common';
 
+import { request } from './request';
+
 const getUserinfo = () => {
-  return axios.get<IRes<UserInfo>>(`/api/user/userInfo`);
+  return request.get(`/api/user/userInfo`).json<IRes<UserInfo>>();
 };
 
 export const userApiKey = {
@@ -15,7 +15,7 @@ export const userApiKey = {
 export const useGetUserInfo = () => {
   return useQuery([userApiKey.getUserinfo], () => getUserinfo(), {
     select: (data) => {
-      return data.data.data;
+      return data.data;
     },
   });
 };
